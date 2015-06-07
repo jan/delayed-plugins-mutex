54d06415d57b94a0fbf778ef61dffe8664f26bf3
Jan Bromberger <jan.bromberger@gmx.de>
2015-06-07 17:36:16 +0200

    Share migration

--- lib/delayed/plugins/mutex/engine.rb
+++ lib/delayed/plugins/mutex/engine.rb
@@ -1,5 +1,13 @@
 module Delayed::Plugins::Mutex
   class Engine < ::Rails::Engine
     isolate_namespace Delayed::Plugins::Mutex
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
