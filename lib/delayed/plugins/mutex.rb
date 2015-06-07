18358a5fdc75cf06a9c85f77941f4453bcdf656d
Jan Bromberger <jan.bromberger@gmx.de>
2015-06-07 17:42:23 +0200

    Fix require

--- lib/delayed/plugins/mutex.rb
+++ lib/delayed/plugins/mutex.rb
@@ -1,6 +1,6 @@
 require 'delayed/plugin'
 
-require "delayed_plugins_mutex/engine"
+require "delayed/plugins/mutex/engine"
 require 'delayed/plugins/mutex/version'
 require 'delayed/plugins/mutex/plugin'
 
