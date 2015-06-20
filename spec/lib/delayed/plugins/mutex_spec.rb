ecbd15c5b0455288983c35ec5cbee5f82c8f6fdc
Jan Bromberger <jan.bromberger@gmx.de>
2015-06-20 14:57:47 +0200

    Added spec stub

---
+++ spec/lib/delayed/plugins/mutex_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Delayed::Plugins::Mutex::Plugin do
+  it 'is defined' do
+    Delayed::Plugins::Mutex::Plugin
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'delayed/plugins/mutex'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
